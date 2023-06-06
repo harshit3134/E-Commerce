980cc710598fd772926e92ddce9ef6b3657f0eb8
andrewbabu <110194640+andrewbabu@users.noreply.github.com>
2023-06-07 00:45:36 +0530

    search functionality

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from home.views import index
+from home.views import index,search
 
 urlpatterns = [
    
     path('' , index , name="index"),
+    path('search/',search,name="search")
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from products.models import Product
+from django.db.models import Q
 
 def index(request):
 
     context = {'products': Product.objects.all()}
     return render(request, 'home/index.html', context)
+
+def search(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    products = Product.objects.filter(
+        Q(category__category_name__icontains = q)|
+        Q(product_name__icontains = q)|
+        Q(author__icontains = q)|
+        Q(product_description__icontains = q)
+        )
+    context = {'products' : products}
+
+    return render(request, 'home/index.html',context)
